5577ed12840b506c85cd0da4980e407c5e735f71
wertasy <wertasy@qq.com>
2023-02-19 10:50:40 +0800

    使用 coolsnowwolf/luci

--- OpenWrt/Redmi-AX6000/diy-part1.sh
+++ OpenWrt/Redmi-AX6000/diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '/luci/d' feeds.conf.default
+echo 'src-git luci https://github.com/coolsnowwolf/luci' >> feeds.conf.default
