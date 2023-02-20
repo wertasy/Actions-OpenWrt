8aeab441c88cdf806fc4cf394ed1e59611a82f29
wertasy <wertasy@qq.com>
2023-02-20 23:32:31 +0800

    add patch

--- OpenWrt/Redmi-AX6000/diy-part1.sh
+++ OpenWrt/Redmi-AX6000/diy-part1.sh
@@ -17,6 +17,11 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/coolsnowwolf/lede tmp/coolsnowwolf/lede
+ln -sf $PWD/tmp/coolsnowwolf/lede/target/linux/generic/hack-5.15/992-add-ndo-do-ioctl.patch target/linux/generic/hack-5.15
+ln -sf $PWD/tmp/coolsnowwolf/lede/target/linux/generic/hack-5.15/953-net-patch-linux-kernel-to-support-shortcut-fe.patch target/linux/generic/hack-5.15
+ln -sf $PWD/tmp/coolsnowwolf/lede/target/linux/generic/hack-5.15/982-add-bcm-fullconenat-support.patch target/linux/generic/hack-5.15
+ln -sf $PWD/tmp/coolsnowwolf/lede/target/linux/generic/hack-5.15/992-add-ndo-do-ioctl.patch target/linux/generic/hack-5.15
+
 echo "src-link coolsnowwolf_lede $PWD/tmp/coolsnowwolf/lede/package/lean" >> feeds.conf.default
 echo 'src-git coolsnowwolf_packages https://github.com/coolsnowwolf/packages' >> feeds.conf.default
 echo 'src-git coolsnowwolf_luci https://github.com/coolsnowwolf/luci' >> feeds.conf.default
